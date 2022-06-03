eac23ac94e88b6f9cae997a08666202bab5d7bdc
ehnat <ewelina.hnat@gmail.com>
2022-06-03 21:51:47 +0200

    Improve tests configuration

--- tests/config/project/config.ts
+++ tests/config/project/config.ts
@@ -1,15 +1,15 @@
 import configData from './configData';
-// import lodash from 'lodash';
+import lodash from 'lodash';
 import yargs from 'yargs';
 
 const commonConfig = configData.common;
 const environment = yargs.argv.env;
 const envConfig = configData[environment];
 
-// const config = lodash.merge(commonConfig, envConfig);
+const config = lodash.merge(commonConfig, envConfig);
 
-const baseUrl: string = commonConfig.baseUrl;
-const taskName: string = envConfig.taskName;
+const baseUrl: string = config.baseUrl;
+const taskName: string = config.taskName;
 
 export {
 baseUrl,
